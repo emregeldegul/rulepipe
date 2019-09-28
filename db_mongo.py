b29847254619e8751b178bbe04c58b493b947148
ileri <celil.ileri@bil.omu.edu.tr>
2019-09-29 00:25:25 +0300

    init Mongo class for RulePipe persistent database

---
+++ db_mongo.py
@@ -0,0 +1,27 @@
+from pymongo import MongoClient
+import json
+class Mongo:
+    def __init__(self, ip="localhost", port=27017, username="", 
+        password="", auth=False, db_name="rulepipe"):
+
+        if auth:
+            connect_url = "mongodb://" + username + ":" + password + \
+                "@" + ip + ":" + str(port)
+        else:
+            connect_url = "mongodb://" + ip + ":" + str(port)
+
+        self.client = MongoClient(connect_url)
+
+        if db_name in self.client.list_database_names():
+            self.db = self.client[db_name]
+        else:
+            print(db_name, " database not exists in", ip)
+
+    def add_rule(self, name, rule):
+        return self.db["rules"].insert_one({"name": name, "rule": str(rule)})
+
+    def get_flow(self, name):
+        flow = []
+        for item in self.db["rules"].find({"name": name}):
+            flow.append(json.loads(item["rule"].replace("\'", "\"")))
+        return flow
