60519e0cdea1ae8d508a31372c2ec00426304165
ileri <celil.ileri@bil.omu.edu.tr>
2019-09-29 00:24:41 +0300

    init LocalDB class

---
+++ db_local.py
@@ -0,0 +1,14 @@
+class LocalDB:
+    def __init__(self):
+        self.db = {}
+
+    def add_rule(self, name, rule):
+        if not name in self.db.keys():
+            self.db[name] = []
+        self.db[name].append(rule)
+
+    def get_flow(self, name):
+        if not name in self.db.keys():
+            print("Rule not found.")
+            return
+        return self.db[name]
