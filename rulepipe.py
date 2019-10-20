a8a58a35a80f00ce13dcc71c915e0c5b0b206dd8
ileri <celil.ileri@bil.omu.edu.tr>
2019-10-21 02:06:48 +0300

    Enable precedence in environment variables #14

--- rulepipe.py
+++ rulepipe.py
@@ -53,10 +53,36 @@ class RuleManager(object):
     def __init__(self):
         logging.info('Rule manager initializing')
         logging.debug('Database client initializing')
+        self.load_environment_variables()
         self.init_database_client()
         
-        if(os.getenv("use_caching") == "True" or os.getenv("use_caching") == "true"):
-            self.init_redis_client()
+        if(self.ENV["USE_CACHE"]):
+            logging.info("Caching active")
+            self.init_redis_client(self.ENV["REDIS_IP"], self.ENV["REDIS_PORT"])
+
+    def load_environment_variables(self):
+        self.load_dotenv_if_defined()
+        self.ENV = {
+            "DB_TYPE"         : str(os.getenv("RULEPIPE_DB_TYPE", "local")).lower(),
+            "DB_IP"           : os.getenv("RULEPIPE_DB_IP", "127.0.0.1"),
+            "DB_PORT"         : int(os.getenv("RULEPIPE_DB_PORT", 27017)),
+            "DB_USER"         : os.getenv("RULEPIPE_DB_USER", "rulepipe"),
+            "DB_PASSWORD"     : os.getenv("RULEPIPE_DB_PASSWORD", ""),
+            "DB_NAME"         : os.getenv("RULEPIPE_DB_NAME", "rulepipe"),
+            "DB_AUTHENTICATE" : json.loads(os.getenv("RULEPIPE_DB_AUTHENTICATE", "False").lower()),
+            "USE_CACHE"       : json.loads(os.getenv("RULEPIPE_USE_CACHE", "False").lower()),
+            "REDIS_IP"        : os.getenv("RULEPIPE_REDIS_IP", "127.0.0.1"),
+            "REDIS_PORT"      : int(os.getenv("RULEPIPE_REDIS_PORT", 6379))
+        }
+
+    def load_dotenv_if_defined(self):
+        env_file = os.getenv("RULEPIPE_ENVFILE")
+        if(env_file):
+            logging.info("Using Environment File : " + str(env_file))
+            load_dotenv(env_file)
+        else:
+            logging.info("Environment file not specified. Looking for in-folder .env file")
+            load_dotenv()
 
     def init_database_client(self):
         """
@@ -65,27 +91,31 @@ class RuleManager(object):
         By default, local (and in-memory) dictionary object is used as a DB.
         In "local" db, it will not be persistent.
         """
-        db              = os.getenv("db_type")
-        db_ip           = os.getenv("db_server")
-        db_port         = os.getenv("db_port")
-        db_user         = os.getenv("db_user")
-        db_pass         = os.getenv("db_pass")
-        db_name         = os.getenv("db_name")
-        db_authenticate = os.getenv("db_authenticate")
-        
-        if db == "local":
+
+        if self.ENV["DB_TYPE"] == "local":
             logging.info("Database type: In-memory database")
             self.db = LocalDB()
-        elif db == "mongo" or db == "mongodb":
+        elif self.ENV["DB_TYPE"] == "mongo" or self.ENV["DB_TYPE"] == "mongodb":
             logging.info("Database type: MongoDB.")
-            if(db_authenticate == 'True' or db_authenticate == 'true' or db_authenticate == '1'):
-                self.db = Mongo(ip=db_ip, port=db_port, username=db_user, password=db_pass, auth=True, db_name=db_name)
+            if(self.ENV["DB_AUTHENTICATE"]):
+                self.db = Mongo(
+                    ip      =self.ENV["DB_IP"], 
+                    port    =self.ENV["DB_PORT"], 
+                    username=self.ENV["DB_USER"], 
+                    password=self.ENV["DB_PASSWORD"], 
+                    auth    =True, 
+                    db_name =self.ENV["DB_NAME"]
+                )
             else:
-                self.db = Mongo(ip=db_ip, port=db_port, db_name=db_name)
+                self.db = Mongo(
+                    ip     =self.ENV["DB_IP"], 
+                    port   =self.ENV["DB_PORT"], 
+                    db_name=self.ENV["DB_NAME"]
+                )
         else:
             logging.error("This database type is not supporting.")
 
-    def init_redis_client(self, host="localhost", port=6379):
+    def init_redis_client(self, host, port):
         self.redis = Redis(host, port)
 
     def add_rule_json_as_string(self, name, rule_string):
@@ -99,7 +129,8 @@ class RuleManager(object):
         Adds a rule into Rule Database as JSON
         """
         self.db.add_rule(name, rule)
-        if(os.getenv("use_caching") == 'True' or os.getenv("use_caching") == 'true'):
+        if(self.ENV["USE_CACHE"]):
+            logging.debug("New rule caching...")
             rule_name_hash = self.md5(name)
             rule_time_hash = self.md5(name + "_cache_time")
             self.redis.set(rule_name_hash, str(rule))
@@ -115,7 +146,8 @@ class RuleManager(object):
         """
         Runs rule using given data and returns the result
         """
-        if(os.getenv("use_caching") == 'True' or os.getenv("use_caching") == 'true'):
+        if(self.ENV["USE_CACHE"] and not self.redis == None):
+            logging.debug("Checking is Redis connected : " + str(self.redis.ping()))
             return self.execute_rule_json_with_caching(name, data)
         else:
             return self.execute_rule_json_without_caching(name, data)
@@ -127,9 +159,11 @@ class RuleManager(object):
         rule_time = self.redis.get(rule_time_hash)
         statement_time = self.redis.get(statement_time_hash)
 
-        if(rule_time == None or statement_time == None):
+        if(
+            rule_time      == None or rule_time      == b'None' or 
+            statement_time == None or statement_time == b'None'
+        ):
             return True
-
         return float(rule_time) > float(statement_time)
 
     def execute_rule_json_with_caching(self, name, data):
@@ -139,19 +173,31 @@ class RuleManager(object):
         statement_time_hash = self.md5(statement_name_hash + "_cache_time")
 
         logging.debug("Checking Rule flow existance in cache with { rule_name_hash: " + rule_name_hash + " , rule_time_hash: " + rule_time_hash + " }")
-        if(self.redis.get(rule_name_hash) == None or self.redis.get(rule_time_hash) == None):
+        
+        if (
+            self.redis.get(rule_name_hash) == b'None' or 
+            self.redis.get(rule_name_hash) ==   None  or 
+            self.redis.get(rule_time_hash) == b'None' or
+            self.redis.get(rule_time_hash) ==   None
+        ):
             logging.debug("Rule flow not found in cache, fetching from db...")
             flow_db_record = self.db.get_flow(name)
-            logging.debug("Rule fetched from db, caching...")
-            self.redis.set(rule_name_hash, str(flow_db_record))
-            self.redis.set(rule_time_hash, str(time.time()))
+            if(flow_db_record):
+                logging.debug("Rule fetched from db, caching...")
+                self.redis.set(rule_name_hash, str(flow_db_record))
+                self.redis.set(rule_time_hash, str(time.time()))
+            else:
+                logging.error("Rule couldnt fetch from db.")
+                return
 
         logging.debug("Rule flow found in cache, fetching...")
-        flow = json.loads(str(self.redis.get(name), 'utf-8').replace("\'", "\""))
-        logging.debug("Rule Flow: " + str(flow))
+        flow = [json.loads(str(self.redis.get(rule_name_hash), 'utf-8').replace("\'", "\""))]
+        logging.debug("Rule Flow: " + str(flow.__class__) + " : " + str(flow))
 
         logging.debug("Checking Statement result existance in cache with { statement_name_hash: " + statement_name_hash + " , statement_time_hash: " + statement_time_hash + " }")
-        if(self.redis.get(statement_name_hash) == None or self.is_cached_statement_updated(rule_time_hash, statement_time_hash)):
+        if(
+            self.redis.get(statement_name_hash) == b'None' or self.redis.get(statement_name_hash) == None or
+            self.is_cached_statement_updated(rule_time_hash, statement_time_hash)):
             logging.debug("rule_time : " + str(self.redis.get(rule_time_hash)) + ", statement_time : " + str(self.redis.get(statement_time_hash)) + "\n")
             logging.debug("Statement not found or become old in cache , executing...")
             response = self.process_steps(flow, data)
@@ -197,30 +243,29 @@ class RuleManager(object):
         return RuleOperations.operations[step["Match"]](results)
 
     def process_steps(self, flow, data):
-        for step in flow:
-            logging.debug("Processing Step: " +  str(step))
+        if(flow):
+            for step in flow:
+                logging.debug("Processing Step: " +  str(step))
 
-            if step["Type"] == "rule":
-                result = self.processRule(step, data)
-                logging.debug("Result: " + str(result))
-                return result
+                if step["Type"] == "rule":
+                    result = self.processRule(step, data)
+                    logging.debug("Result: " + str(result))
+                    return result
 
-            elif step["Type"] == "ruleset":
-                logging.debug("RuleSet found, continuing recursively.")
-                rulesetResults = []
-                for rule in step["Rules"]:
-                    logging.debug("Rule sent #149:" + str(rule) + str(type(rule)))
-                    rulesetResults.append(self.process_steps([rule], data))
-                return RuleOperations.operations[step["Match"]](rulesetResults)
-
+                elif step["Type"] == "ruleset":
+                    logging.debug("RuleSet found, continuing recursively.")
+                    rulesetResults = []
+                    for rule in step["Rules"]:
+                        logging.debug("Rule sent #149:" + str(rule) + str(type(rule)))
+                        rulesetResults.append(self.process_steps([rule], data))
+                    return RuleOperations.operations[step["Match"]](rulesetResults)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
-    load_dotenv()
 
     rules = RuleManager()
     
-    rules.add_rule_json_as_string("guray2", """
+    rules.add_rule_json_as_string("guray6", """
     {
         "Type": "ruleset",
         "Match": "all",
@@ -232,7 +277,7 @@ if __name__ == "__main__":
                     {
                         "field": "responseTimeInSeconds",
                         "condition": "lte",
-                        "value": 0.455
+                        "value": 0.45
                     },
                     {
                         "field": "statusCode",
@@ -262,9 +307,9 @@ if __name__ == "__main__":
     """)
     
 
-    print(rules.execute_rule_json_as_string("guray2", """
+    print(rules.execute_rule_json_as_string("guray6", """
     {
-        "responseTimeInSeconds": 1,
+        "responseTimeInSeconds": 0.1,
         "statusCode": 200,
         "details": {
             "name": "mymetric",
